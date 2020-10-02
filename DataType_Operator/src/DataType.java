ecfa70f7ef001f94d3dbb9fc0f85aacf8e2c7b65
zhujiale <379105244@qq.com>
2020-10-02 19:16:59 +0800

    1.添加学习 byte类型，short类型，String类型，final关键字

--- DataType_Operator/src/DataType.java
+++ DataType_Operator/src/DataType.java
@@ -1,7 +1,5 @@
-import java.util.logging.Logger;
-
 public class DataType {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //整型变量 大小为4 与操作系统无关
         /*int num = 10;
         System.out.println(num);//整型变量定义和初始化
@@ -52,9 +50,75 @@ public class DataType {
         //2.计算机中的字符本质是一个整数，在C语言中使用ASCII表示字符。而Java中使用Unicode表示字符
         //3.因此一个字符占用两个字节，表示字符种类更多，包括中文
 
-        char ch = '呵';
+       /* char ch = '呵';
         System.out.println(ch);
-        System.out.println(Character.SIZE/8);
+        System.out.println(Character.SIZE / 8);*/
 
+        //字节类型变量
+        //注意：
+        //1.字节类型表示的也是整数，只占一个字节，表示范围较小（-128~+127）
+        //2.字节类型和字符类型互不相干
+
+       /* byte value  = 0;
+        System.out.println(value);*/
+
+        //短整型变量
+        //注意事项：
+        //1.short占用2个字节，表示的数据范围是（-32768~+32767）
+        /*short value = 0;
+        System.out.println(value);
+        System.out.println(Short.SIZE/8);*/
+
+        //bool类型变量
+        //1.boolean 类型的变量只有两种取值，true表示真，false表示假
+        //2.Java的boolean 类型和int不能相互转换，不存在1表示true，0表示false这样的用法
+        //3.boolean 类型有些JVM的实现是占1个字节，有些是占1个比特位，这个没有明确规定
+      /*  boolean value = true;
+        System.out.println(value);*/
+
+        /*boolean value = true;
+        System.out.println(value+1);
+
+        编译错误：
+        java: 二元运算符 '+' 的操作数类型错误
+        第一个类型:  boolean
+        第二个类型: int*/
+
+
+        //字符串类型变量
+        //注意事项：
+        //1.Java使用双引号+若干字符的方式表示字符串字面值
+        //2.和之前类型不同的是，String不是基本类型，而是引用类型
+        //3.字符串中的一些特定的不太方便直接表示的字符需要进行转义
+        /*String name = "zhangsan";
+        System.out.println(name);*/
+
+        /*String name = "My name is \"zhangsan\"";
+        System.out.println(name);*/
+
+        //字符串的+操作，表示字符拼接
+        /*String a = "hello";
+        String b = "world";
+        String c = a + b;
+        System.out.println(c);*/
+
+        //字符串和整数拼接
+        /*String str = "result = ";
+        int a = 10;
+        int b = 20;
+        String result = str + a + b;
+        System.out.println(result);*/
+
+        /*int a = 10;
+        int b = 20;
+        System.out.println("a = " + a + ",b = " + b);*/
+
+
+        //final
+        //常量在运行过程中不允许被修改
+        /*final int a = 10;
+        a = 20;
+
+        java: 无法为最终变量a分配值*/
     }
 }
