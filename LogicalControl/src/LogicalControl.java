05cffbc74adf042e30e8f78878c753a4544879b7
zhujiale <379105244@qq.com>
2020-10-30 18:34:09 +0800

    添加： 1.do while 2.输入输出    System.in.read    Scanner 3.猜数字游戏

--- LogicalControl/src/LogicalControl.java
+++ LogicalControl/src/LogicalControl.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * @Author : Jaler
  * @Date : 2020/10/28 11:30
@@ -107,7 +112,7 @@ public class LogicalControl {
         System.out.println(result);*/
 
         //计算 1！+2！+3！+4！+5!
-        int num = 1;
+        /*int num = 1;
         int sum = 0;
         while (num <= 5) {
             int factorResult = 1;
@@ -119,6 +124,74 @@ public class LogicalControl {
             sum += factorResult;
             num++;
         }
+        System.out.println("sum = " + sum);*/
+
+
+        //do while
+       /* int num=1;
+        do{
+            System.out.println(num);
+            num++;
+        }while (num<=10);*/
+
+        //输入输出
+        /*String msg="nihao";
+        System.out.println(msg);//输出一个字符串，带换行
+        System.out.print(msg);//输出一个字符串，不带换行
+        System.out.printf("%s",msg);//格式化输出*/
+
+        //从键盘输入
+        //直接使用 System.in.read 可以读入一个字符. 但是需要搭配异常处理
+
+        /*try {
+            System.out.print("Enter a Char:");
+            char i = (char) System.in.read();
+            System.out.println("your char is :" + i);
+        } catch (IOException e) {
+            System.out.println("exception");
+        }*/
+
+        //使用Scanner读取字符串/整数/浮点数
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("请输入你的姓名：");
+        String name = sc.nextLine();
+        System.out.println("请输入你的年龄：");
+        int age = sc.nextInt();
+        System.out.println("请输入你的工资：");
+        float salary = sc.nextFloat();
+        System.out.println("你的信息如下：");
+        System.out.println("姓名：" + name + "\n" + "年龄：" + age + "\n" + "工资：" + salary);
+        sc.close();*/
+
+        //使用Scanner循环读取N个数字
+        /*Scanner sc = new Scanner(System.in);
+        double sum = 0.0;
+        int num = 0;
+        while (sc.hasNextDouble()) {
+            double tmp = sc.nextDouble();
+            sum += tmp;
+            num++;
+        }
         System.out.println("sum = " + sum);
+        System.out.println("avg = " + sum / num);
+        sc.close();*/
+
+        //猜数字游戏
+        Random random = new Random();//默认随机种子是系统时间
+        Scanner sc = new Scanner(System.in);
+        int toGuess = random.nextInt(100);
+        while (true) {
+            System.out.println("请输入要输入的数字：(1-100)");
+            int num = sc.nextInt();
+            if (num < toGuess) {
+                System.out.println("低了");
+            } else if (num > toGuess) {
+                System.out.println("高了");
+            } else {
+                System.out.println("猜对了");
+                break;
+            }
+        }
+        sc.close();
     }
 }
