cfe688c74345b7959d5182b4c853dd90b9d3a208
zhujiale <379105244@qq.com>
2020-11-05 17:01:03 +0800

    更新学习： 1.递归 2.递归练习

---  UseOfMethods/src/UseOfMethods.java
+++  UseOfMethods/src/UseOfMethods.java
@@ -44,7 +44,7 @@ public class UseOfMethods {
         System.out.println("arr[0] = " + arr[0] + " arr[1] = " + arr[1]);*/
 
         //方法重载
-        int a = 10;
+        /*int a = 10;
         int b = 20;
         int ret = add(a, b);
         System.out.println("ret = " + ret);
@@ -58,9 +58,72 @@ public class UseOfMethods {
         double b3 = 10.5;
         double c3 = 20.5;
         double ret3 = add(a3, b3, c3);
-        System.out.println("ret3 = " + ret3);
+        System.out.println("ret3 = " + ret3);*/
+
+        //方法递归
+        /*int n = 5;
+        int ret = factor1(n);
+        System.out.println("ret = " + ret);*/
+
+        //递归练习
+        //print(1234);
+        //System.out.println(sum(10));
+        //System.out.println(sum2(1729));
+        //System.out.println(fib(10));
+        System.out.println(fib2(10));
     }
 
+    //求斐波那契数列的第 N 项 迭代
+    public static int fib2(int n) {
+        int first = 1;
+        int second = 1;
+        for (int i = 3; i <= n; i++) {
+            int tmp = first + second;
+            first = second;
+            second = tmp;
+        }
+        return second;
+    }
+
+    //求斐波那契数列的第 N 项 递归
+    public static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    //写一个递归方法，输入一个非负整数，返回组成它的数字之和.
+    public static int sum2(int num) {
+        if (num < 10) {
+            return num;
+        }
+        return num % 10 + sum2(num / 10);
+    }
+
+    //递归求1+2+3+···+10
+    public static int sum(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return num + sum(num - 1);
+    }
+
+    //按顺序打印一个数字的每一位数
+    public static void print(int num) {
+        if (num > 9) {
+            print(num / 10);
+        }
+        System.out.println(num % 10);
+    }
+
+    //方法递归
+    public static int factor1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factor1(n - 1);
+    }
 
     //采用引用类型参数
     public static void swap2(int[] arr) {
