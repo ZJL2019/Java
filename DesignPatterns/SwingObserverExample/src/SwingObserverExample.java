440fbdec734628d603ec619dff7bfb08d98756bf
zhujiale <379105244@qq.com>
2020-12-03 15:37:05 +0800

    SwingObserverExample

---
+++ DesignPatterns/SwingObserverExample/src/SwingObserverExample.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @Author : Jaler
+ * @Date : 2020/11/30 14:24
+ * @describe :
+ * @Version : 1.0
+ */
+public class SwingObserverExample {
+    JFrame frame;
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+    public void go(){
+        frame=new JFrame();
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+
+        frame.getContentPane().add(BorderLayout.CENTER,button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER,button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    class AngelListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
+
+    class DevilListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Come on, do it!");
+        }
+    }
+
+}
